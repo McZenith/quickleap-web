1db8fdde818ed9365f02aa8f726b119dc02145ea
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-04-27 17:23:07 +0100

    NavBar v07

--- components/Header/imports.js
+++ components/Header/imports.js
@@ -4,25 +4,12 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     height: 15,
   },
-  toolbar: {
-    minHeight: 70,
-  },
 }));
 
-export {
-  useTheme,
-  useStyles,
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  useMediaQuery,
-};
+export { useStyles, AppBar, Toolbar, Typography, Button };
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,12 @@
-import { useStyles, AppBar, Toolbar, useMediaQuery, useTheme } from "./imports";
+import { useStyles, AppBar, Toolbar } from "./imports";
 
 const Header = () => {
   const { root, toolbar } = useStyles();
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
     <div className={root}>
       <AppBar position="static" color="white" elevation={3}>
-        <Toolbar className={matches ? toolbar : null}></Toolbar>
+        <Toolbar></Toolbar>
       </AppBar>
     </div>
   );
